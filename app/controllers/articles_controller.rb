8d238a5d18f6941cea6bde7f88196d3529d3c0ad
William Morales <william.rolando88@hotmail.com>
2022-11-29 12:30:36 -0500

    Add basic request authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret", except: %i[show, index]
   def index
     @articles = Article.all
   end
